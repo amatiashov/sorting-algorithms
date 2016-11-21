413db0907f1f4afdaa7fb5ea448ab4160365ec02
Artyom Matyashov <smart7797@gmail.com>
2016-11-21 20:10:38 +0300

    Create Sorter interface

---
+++ src/sort/Sorter.java
@@ -0,0 +1,19 @@
+package sort;
+
+/**
+ * Created by Artyom Matyashov <smart7797@gmail.com> on 21/11/2016.
+ */
+public interface Sorter {
+    /**
+     * Метод выполняет сортрировку массива - Integer[]
+     * Сортировка может выполняться следующими алгоритмами:
+     *      - BubbleSort
+     *      - QuickSort
+     *      - MergeSort
+     *      - ShellSort
+     *
+     * @param array     неотсартированный входной массив массив
+     *
+     */
+    void Sort(Integer[] array);
+}
